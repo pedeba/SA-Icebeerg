ae1acc67feff5001f80df2d8fe37e6614ae04db4
Pedro de Barros <pedropedeba@gmail.com>
2020-10-05 20:29:57 -0300

    edicao de senha adicionada

--- js/dados.js
+++ js/dados.js
@@ -2,6 +2,8 @@ let usuarios = JSON.parse(localStorage.getItem("users"))
 let userlog = JSON.parse(localStorage.getItem("pessoalogada"))
 let nomeEditInput = document.getElementById("nomeEdit")
 let userEditInput = document.getElementById("userEdit")
+let pass1EditInput = document.getElementById("pass1Edit")
+let pass2EditInput = document.getElementById("pass2Edit")
 mostraDados.innerHTML=`Nome: ${userlog.nome} <br> Usuario: ${userlog.usuario} `
 
 function editarDados(){
@@ -20,10 +22,29 @@ function editarDados(){
                 }  
             }
         })
-        localStorage.users = JSON.stringify(usuarios)
-        localStorage.pessoalogada = JSON.stringify(userlog)
-        window.location.reload()
+        attLocalStorage()
     }
-
 }
 
+function editarSenha() {
+    if(!pass1EditInput.value || !pass2EditInput.value){
+        alert("Não deixe Campos vazios!")
+    } else if (pass1EditInput.value !== pass2EditInput.value){
+        alert("As senhas estão diferentes")
+    } else {
+        usuarios.forEach((user)=>{
+            if(user.usuario == userlog.usuario){
+                user.senha = pass1EditInput.value
+                userlog.senha = pass1EditInput.value
+            }
+        })
+    }
+    alert("Senha alterada com sucesso!")
+    attLocalStorage()
+}
+
+function attLocalStorage(){
+    localStorage.users = JSON.stringify(usuarios)
+    localStorage.pessoalogada = JSON.stringify(userlog)
+    window.location.reload()      
+}
