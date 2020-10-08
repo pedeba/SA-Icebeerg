e9e60f4e348582bdd0466e8999e1ae192d8a92f4
Pedro de Barros <pedropedeba@gmail.com>
2020-10-08 20:49:53 -0300

    produtos na home

---
+++ js/index.js
@@ -0,0 +1,13 @@
+let cervejasLista = document.getElementById("cervejas")
+let produtos = JSON.parse(localStorage.produtos)
+
+for(let i = 0; i<produtos.length; i++){
+    cervejasLista.appendChild(document.createElement("div")).classList.add("div-cerveja")
+}
+let cervejas = document.querySelectorAll(".div-cerveja")
+cervejas.forEach(function(div, index){
+    div.appendChild(document.createElement('h3')).innerText = produtos[index].nome
+    div.appendChild(document.createElement('p')).innerText = produtos[index].tipo
+    div.appendChild(document.createElement('p')).innerText = produtos[index].descricao
+    // div.appendChild(document.createElement('button')).innerText = 'Mais informações'
+})
